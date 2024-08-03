c8c2a525ea86e7198c58744aad344827ee1bc7d5
Narapady Chhuoy <narapady.chhuoy@enfi.ai>
2024-08-03 09:28:41 -0400

    Revert "delete disabled plugin"
    This reverts commit 039ece6d3e5b131eaccd34c840465a4c69338c1f.

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -1,5 +1,21 @@
 return {
   { "akinsho/bufferline.nvim",          enabled = false },
+  -- { "folke/persistence.nvim", enabled = false },
+  -- { "folke/trouble.nvim", enabled = false },
   { "linux-cultist/venv-selector.nvim", enabled = false },
   { "stevearc/conform.nvim",            enabled = false },
+  {
+    "folke/styler.nvim",
+    priority = 1000,
+    enabled = false,
+    config = function()
+      require("styler").setup({
+        themes = {
+          markdown = { colorscheme = "tokyonight" },
+        },
+      })
+    end,
+  },
+  -- { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  -- { "nvimdev/dashboard-nvim", enabled = false },
 }
